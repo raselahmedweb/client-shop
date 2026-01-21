ea1ba578405e06d68893998c5d1a2e2bab4ab827
Rasel Ahmed <raselwebdev@hotmail.com>
2026-01-21 06:05:45 +0300

    Redux Add

--- components/login/signup.tsx
+++ components/login/signup.tsx
@@ -1,44 +1,69 @@
-
 import Button from "@/components/ui/Button";
-import Input from "@/components/ui/Input";
 import { ThemeContext } from "@/context/ThemeProvider";
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { useContext } from "react";
-import { Image, KeyboardAvoidingView, Platform, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React, { useContext } from "react";
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 // Media
-const bubble2 =require("@/assets/bubble/bubble2.png");
-const bubble3 =require("@/assets/bubble/bubble3.png");
+const bubble2 = require("@/assets/bubble/bubble2.png");
+const bubble3 = require("@/assets/bubble/bubble3.png");
 
 export default function Signup() {
   const { theme, colorScheme } = useContext(ThemeContext);
   const styles = createStyle(theme, colorScheme);
 
+  const [text, onChangeText] = React.useState("");
+
   return (
     <SafeAreaView style={styles.safeArea}>
-        {/* Decorative Background Bubbles */}
-        <Image source={bubble2} style={styles.bubble2} />
-        <Image source={bubble3} style={styles.bubble3} />
+      {/* Decorative Background Bubbles */}
+      <Image source={bubble2} style={styles.bubble2} />
+      <Image source={bubble3} style={styles.bubble3} />
       <KeyboardAvoidingView
         style={styles.container}
         behavior={Platform.OS === "ios" ? "padding" : "height"}
       >
-        
         <View style={styles.headingContainer}>
           <Text style={styles.title}>Create</Text>
           <Text style={styles.title}>Account</Text>
         </View>
 
         <View style={styles.form}>
-          <Input placeholder="Full Name" />
-          <Input placeholder="Email" />
+          <TextInput
+            style={{
+              height: 50,
+              borderColor: "gray",
+            }}
+            onChangeText={onChangeText}
+            value={text}
+          />
+          {/* <Input placeholder="Email" />
           <Input placeholder="Password" />
-          <Input placeholder="Phone number" />
+          <Input placeholder="Phone number" /> */}
         </View>
 
         <View style={styles.actions}>
-          <Button txt="Next" bg={theme.primary} color="#fff" />
-          <Link href="/" style={styles.cancelLink}>Cancel</Link>
+          <Button
+            onPress={() => {
+              /* Handle signup logic here */
+              console.log(text);
+            }}
+            txt="Next"
+            bg={theme.primary}
+            color="#fff"
+          />
+          <Link href="/" style={styles.cancelLink}>
+            Cancel
+          </Link>
         </View>
 
         <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
@@ -55,7 +80,7 @@ function createStyle(theme, colorScheme) {
     },
     container: {
       flex: 1,
-      justifyContent: 'flex-end',
+      justifyContent: "flex-end",
       alignItems: "center",
       paddingHorizontal: 24,
       paddingBottom: 40,
@@ -64,7 +89,7 @@ function createStyle(theme, colorScheme) {
     headingContainer: {
       width: "100%",
       marginBottom: 40,
-      zIndex: 3
+      zIndex: 3,
     },
     title: {
       fontSize: 42,
@@ -80,13 +105,13 @@ function createStyle(theme, colorScheme) {
     form: {
       width: "100%",
       marginBottom: 30,
-      zIndex: 3
+      zIndex: 3,
     },
     actions: {
       width: "100%",
       alignItems: "center",
       gap: 20,
-      zIndex: 3
+      zIndex: 3,
     },
     cancelLink: {
       fontSize: 18,
@@ -116,6 +141,6 @@ function createStyle(theme, colorScheme) {
       bottom: -10,
       right: -10,
       zIndex: 1,
-    },  
-});
+    },
+  });
 }
